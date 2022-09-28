1ce6c58d469ab418c3a2a81466adcb11c953da1d
JericoFX <56100605+JericoFX@users.noreply.github.com>
2022-09-28 01:09:58 -0300

    Update main.lua
    a little of sql optimization, need testing, if you need explanation about the sql queries just pm me and i will explain you.
    No more select the entire table just to take the mods, in this case the query will select the "lock" inside the mods table and extract only that information, if is null will return 0 IFNULL(JSON_VALUE(v.mods,'$.lock'),0)

--- server/main.lua
+++ server/main.lua
@@ -2,35 +2,28 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local VehicleList = {}
 
 local function ChangeLocks(plate)
-	MySQL.query('SELECT * FROM player_vehicles WHERE plate = ?', {plate}, function(result)
-		if result[1] then
-			local lock = result[1].lock
-			if lock then
-				lock = lock + 1
-			else
-				lock = 4321
-			end
-			MySQL.update('UPDATE player_vehicles SET lock = ? WHERE plate = ?', {lock, plate})
-		end
-	end)
+	MySQL.single("SELECT IFNULL(JSON_VALUE(p.mods,'$.lock'),0) AS 'lock' FROM player_vehicles p WHERE plate = ?", { plate }
+		,
+		function(result)
+			local mods = tonumber(result.lock) == 0 and 4321 or tonumber(result.lock)
+			MySQL.update("UPDATE player_vehicles SET player_vehicles.mods = JSON_SET(player_vehicles.mods,'$.lock',?) WHERE plate = ?"
+				, { mods, plate })
+		end)
 end
 
 local function GiveKey(plate, model, player, src)
-	MySQL.query('SELECT * FROM player_vehicles WHERE plate = ?', {plate}, function(result)
-		if result[1] then
-			local lock = result[1].lock
+	MySQL.single("SELECT IFNULL(JSON_VALUE(p.mods,'$.lock'),0) AS 'lock' FROM player_vehicles p WHERE plate = ?", { plate }
+		,
+		function(result)
+			local mods = tonumber(result.lock)
 			local info = {}
-			if lock then
-				info.lock = lock
-				info.plate = plate
-				info.model = model
-				player.Functions.AddItem('vehiclekey', 1, nil, info)
-				TriggerClientEvent('QBCore:Notify', src, Lang:t("message.key_received"), 'success')
-			else
-				TriggerClientEvent('QBCore:Notify', src, Lang:t("message.not_initialized"), 'error')
-			end
-		end
-	end)
+			if mods == 0 then TriggerClientEvent('QBCore:Notify', src, Lang:t("message.not_initialized"), 'error') return end
+			info.lock = mods
+			info.plate = plate
+			info.model = model
+			player.Functions.AddItem('vehiclekey', 1, nil, info)
+			TriggerClientEvent('QBCore:Notify', src, Lang:t("message.key_received"), 'success')
+		end)
 end
 
 RegisterNetEvent('qb-vehiclekeys:server:BuyVehicle', function(plate, model)
@@ -43,10 +36,10 @@ end)
 
 RegisterNetEvent('qb-vehiclekeys:server:GiveTempKey', function(plate)
 	local src = source
-    local citizenid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
+	local citizenid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
 
-    if not VehicleList[plate] then VehicleList[plate] = {} end
-    VehicleList[plate][citizenid] = true
+	if not VehicleList[plate] then VehicleList[plate] = {} end
+	VehicleList[plate][citizenid] = true
 	TriggerClientEvent('QBCore:Notify', src, Lang:t("message.temp_key_received"))
 
 end)
@@ -57,7 +50,7 @@ RegisterNetEvent('qb-vehiclekeys:server:ChangeLocks', function(data)
 	local plate = data.plate
 	local cashBalance = Player.PlayerData.money["cash"]
 
-    if Player then
+	if Player then
 		if cashBalance >= Config.ResetPrice then
 			Player.Functions.RemoveMoney("cash", Config.ResetPrice, "Reset-Locks")
 			ChangeLocks(plate)
@@ -76,7 +69,7 @@ RegisterNetEvent('qb-vehiclekeys:server:GiveKey', function(data)
 	local model = data.model
 	local cashBalance = Player.PlayerData.money["cash"]
 
-    if Player then
+	if Player then
 		if cashBalance >= Config.KeyPrice then
 			Player.Functions.RemoveMoney("cash", Config.KeyPrice, "Get-Key")
 			GiveKey(plate, model, Player, src)
@@ -89,26 +82,30 @@ end)
 QBCore.Functions.CreateCallback('qb-vehiclekeys:server:HasKey', function(source, cb, plate)
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
-    local citizenid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
+	local citizenid = QBCore.Functions.GetPlayer(src).PlayerData.citizenid
 	local ok = false
-    if Player then
+	if Player then
 		if VehicleList[plate] and VehicleList[plate][citizenid] then
-			cb(true)				
+			cb(true)
 		else
-			MySQL.query('SELECT * FROM player_vehicles WHERE plate = ?', {plate}, function(result)
-				if result[1] then
-					local lock = result[1].lock
+			MySQL.single("SELECT IFNULL(JSON_VALUE(v.mods,'$.lock'),0) AS vlock FROM  player_vehicles v  WHERE v.plate = ?",
+				{ plate },
+				function(result)
+					if tonumber(result.vlock) == 0 then
+						cb(false)
+					end
+					local mods = tonumber(result.vlock)
 					local items = Player.Functions.GetItemsByName('vehiclekey')
 					if items then
 						for _, v in pairs(items) do
-							if v.info.plate == plate and v.info.lock == lock then
+							print(type(result.vlock), type(v.info.lock), result.vlock, v.info.lock)
+							if v.info.plate == plate and v.info.lock == mods then
 								ok = true
 							end
 						end
 					end
-				end
-				cb(ok)		
-			end)	
+					cb(ok)
+				end)
 		end
 	end
 end)
